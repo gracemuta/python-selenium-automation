b9b8859953199bd180f71d402473954cff476376
grace <graciou@gmail.com>
2022-02-09 13:07:11 -0500

    changes

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("C:\\Users\\artby\\Desktop\\Automation\\python-selenium-automation\\chromedriver.exe")
+
 driver.maximize_window()
 
 # open the url
